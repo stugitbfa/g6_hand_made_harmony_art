38cbab5d607c98bc4af7980f74fa608c05302319
stugitbfa <stu.github@bharatfuturistai.com>
2025-06-23 09:52:45 +0530

    from start

--- apps/web/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', login, name='login'),
-    path('index/', index, name='index'),
-    path('register/', register, name='register'),
-    path('email-verify/', email_verify, name='email_verify'),
-]
