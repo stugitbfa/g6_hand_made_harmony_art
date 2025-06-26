2f12702eb4c3d1ffb479180d066951a24122cd9c
Roshnikushwaha41 <amarsinghkushwahaamarsingh262@gmail.com>
2025-06-26 15:40:52 +0530

    Delete apps directory

--- apps/web/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from apps.web.models import *
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['image', 'caption', 'is_public']
